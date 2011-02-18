59fe7b5a3288ed0b1be8b8e2150f6e3c36223c9e
Liran Nuna <liranuna@gmail.com>
2011-02-17 21:23:48 -0800

    Better tests for conversion issues

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,7 @@
 #include "tests/vec4.h"
 
+#include <cmath>
+#include <limits>
 #include <stdio.h>
 #include <assert.h>
 
@@ -25,10 +27,38 @@ uint32_t vec4_to_color32(__m128 m)
 */
 
 #include "vec4.h"
+#include "dvec4.h"
+#include "ivec4.h"
+#include "uvec4.h"
+#include "bvec4.h"
 
 void printv(const vec4 &v)
 {
 	printf("%f, %f, %f, %f\n", v.x, v.y, v.z, v.w);
+}
+
+void printv(const dvec4 &v)
+{
+	printf("%f, %f, %f, %f\n", v.x, v.y, v.z, v.w);
+}
+
+void printv(const ivec4 &v)
+{
+	printf("%d, %d, %d, %d\n", v.x, v.y, v.z, v.w);
+}
+
+void printv(const uvec4 &v)
+{
+	printf("%u, %u, %u, %u\n", v.x, v.y, v.z, v.w);
+}
+
+void printv(const bvec4 &v)
+{
+	printf("%s, %s, %s, %s\n",
+		v.x ? "true" : "false",
+		v.y ? "true" : "false",
+		v.z ? "true" : "false",
+		v.w ? "true" : "false");
 }
 
 int main()
@@ -39,31 +69,73 @@ int main()
 	tests::vec4::testSwizzleWrite();
 	tests::vec4::testUnary();
 
-	vec4 v(4,3,2,1);
-	vec4 res(1, 2, 3, 4);
+#define VECTOR vec4
+
+	VECTOR v  (4, 3, 2, 1);
+	VECTOR res(1, 2, 3, 4);
+
+	v.xyzw = res.wzyx;
+	assert(v == VECTOR(4, 3, 2, 1));
+
+	v.xyzw = res.wzyx.xxyy;
+	assert(v == VECTOR(4, 4, 3, 3));
 
 	v.xyzw = res;
 	assert(v == res);
 
-	res = vec4(5, 6, 7, 8);
-	assert(res == vec4(5, 6, 7, 8));
+	res = VECTOR(5, 6, 7, 8);
+	assert(res == VECTOR(5, 6, 7, 8));
 
-	v.wzyx = vec4(0.f, -0.f, 1.f, -1.f);
-	assert(v == vec4(-1.f, 1, -0.f, 0.f));
+	v.wzyx = VECTOR(0, 2, 1, 3);
+	assert(v == VECTOR(3, 1, 2, 0));
 
 	v.wzyx = res.xyxy;
-	assert(v == vec4(6, 5, 6, 5));
+	assert(v == VECTOR(6, 5, 6, 5));
 
 	v.wzyx = res.xxxx.yyyy;
-	assert(v == vec4(5, 5, 5, 5));
+	assert(v == VECTOR(5, 5, 5, 5));
+
+	v.wzyx.wzyx = VECTOR(1, 2, 3, 4);
+	assert(v == VECTOR(1, 2, 3, 4));
+
+	v.wzyx.wzyx = v.xxxx;
+	assert(v == VECTOR(1, 1, 1, 1));
+
+	v.xyzw.xyzw = VECTOR(1, 2, 3, 4);
+	assert(v == VECTOR(1, 2, 3, 4));
+
+	v.wzyx.wzyx = v.xyzw.xxxx;
+	assert(v == VECTOR(1, 1, 1, 1));
+
+	res.wzyx.y += 1;
+	assert(res == VECTOR(5, 6, 8, 8));
+
+	res.wzyx[0] += 1;
+	assert(res == VECTOR(5, 6, 8, 9));
+
+	res.wzyx += res.xyxy;
+	assert(res == VECTOR(14, 14, 11, 11));
+
+	res = clamp(res.zxwy, 12, 13);
+	assert(res == VECTOR(12, 13, 12, 13));
+
+	res = floor(res.xyzw);
+	assert(res == VECTOR(12, 13, 12, 13));
+
+	res = res.xxxx + res.xyzw;
+	assert(res == VECTOR(24, 25, 24, 25));
+
+	res = res.xyzw + res.zwxy;
+	assert(res == VECTOR(48, 50, 48, 50));
 
 		// const correctness
-	const vec4 c(1, 2, 3, 4);
-	assert(c == vec4(1, 2, 3, 4));
-	assert(c.xxxx == vec4(1, 1, 1, 1));
-	assert(c.xxxx.x == 1.0f);
-	assert(c.wyzx == vec4(4, 2, 3, 1));
-	assert(c.wyzx.xxxx == vec4(4, 4, 4, 4));
+	const VECTOR c(1, 2, 3, 4);
+	assert(c == VECTOR(1, 2, 3, 4));
+	assert(c.xxxx == VECTOR(1, 1, 1, 1));
+	assert(c.xxxx.x == 1);
+	assert(c.xyzw == c.xyzw);
+	assert(c.wyzx == VECTOR(4, 2, 3, 1));
+	assert(c.wyzx.xxxx == VECTOR(4, 4, 4, 4));
 	assert(c.wyzx.xzyw == c.wzyx);
 	assert(c.wyzx.wyzx == c);
 	assert(c.wyzx.wyzx.wyzx == c.wyzx);
